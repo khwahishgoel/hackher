567a3f25ed0843014364907cfd736afeb41f835b
Khwahish Goel <goelkhwahish@gmail.com>
2026-02-21 12:31:06 -0500

    Add volunteer.py from src folder

---
+++ src/volunteer.py
@@ -0,0 +1,4 @@
+babysitter_form="https://docs.google.com/forms/d/e/1FAIpQLSeunz105rqhmMs_pz6e9iGEG92phh9QwsAQdOF0nYVi3SEBOw/viewform?usp=sharing&ouid=100488612782603083613"
+
+def get_link():
+    return babysitter_form
